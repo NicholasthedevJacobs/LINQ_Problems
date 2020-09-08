279bd73440c6f3dbcf22d710351d0c291309d77d
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-09-08 11:51:57 -0500

    Finished the logic for the second problem.

--- LINQ Problems/Program.cs
+++ LINQ Problems/Program.cs
@@ -10,12 +10,24 @@ namespace LINQ_Problems
     {
         static void Main(string[] args)
         {
+            //FIRST PROBLEM
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             var wordsThatContainTH = words.Where(w => w.Contains("th")); 
             foreach(var word in wordsThatContainTH)
             {
-                Console.WriteLine(words);
+                Console.WriteLine(word);
             }
+           
+
+
+            //SECOND PROBLEM
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var listOfNamesNoDupes = names.Where(n => n.Contains(n));
+            foreach(var name in listOfNamesNoDupes.Distinct())
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
         }
     }
 }
