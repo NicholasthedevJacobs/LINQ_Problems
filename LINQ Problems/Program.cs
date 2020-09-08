0030dbb78667724c03297e1017822a3fd134a3a5
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-09-08 17:20:47 -0500

    Did the logic for the third problem.  Not 100% sure on exactly whats happening.  Might come back and try to solve it another way.

--- LINQ Problems/Program.cs
+++ LINQ Problems/Program.cs
@@ -22,12 +22,30 @@ namespace LINQ_Problems
 
             //SECOND PROBLEM
             List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
-            var listOfNamesNoDupes = names.Where(n => n.Contains(n));
-            foreach(var name in listOfNamesNoDupes.Distinct())
+            var listOfNamesNoDupes = names.Distinct();
+            foreach (var name in listOfNamesNoDupes)
             {
                 Console.WriteLine(name);
             }
+           
+
+            //THIRD PROBLEM
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+
+            var listOfGrades = classGrades.Select(c => c.Split(',')).Select(c => c.
+            Select(grade => double.Parse(grade)).OrderBy(grade => grade).
+            Skip(1)).SelectMany(c => c).Average();
+
+            Console.WriteLine(listOfGrades);
             Console.ReadLine();
+
+
         }
     }
 }
